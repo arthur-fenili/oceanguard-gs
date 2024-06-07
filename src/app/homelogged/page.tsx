0f1167f4681f96e759a9a1fa7795ba240931f927
Arthur Fenili <arthur.fenili@hotmail.com>
2024-06-07 18:51:45 -0300

    integração com backend, e remoção da chave da api hardcoded

--- src/app/homelogged/page.tsx
+++ src/app/homelogged/page.tsx
@@ -1,9 +1,22 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+export default function HomeLoggedPage() {
+    const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        const storedEmail = localStorage.getItem('clienteEmail');
+        if (storedEmail) {
+            setEmail(storedEmail);
+        }
+    }, []);
+
   return (
     <div className="flex flex-col items-center bg-[#f5f0e1] text-[#1e3d59] p-4 pb-10">
-      <h1 className="text-4xl m-9 text-blue-500 text-center">Projeto CleanSea</h1>
+      <span className="text-2xl text-black">Olá, {email}, seja bem-vindo!</span>
+      <h1 className="text-4xl m-9 text-[#ffcb77] text-center">Projeto CleanSea</h1>
       <div className="flex flex-col lg:flex-row items-center justify-center mx-auto max-w-5xl space-y-10 lg:space-y-0 lg:space-x-10">
         <Image src="/colectors-simulation.webp" alt="CleanSea" width={500} height={300} className="w-full lg:w-auto" />
         <div className="flex flex-col items-center justify-center p-4 lg:max-w-md">
@@ -17,7 +30,7 @@ export default function Home() {
 
       <br />
 
-      <h2 className="text-3xl text-blue-500 text-center">Benefícios e Impacto do projeto</h2>
+      <h2 className="text-3xl text-[#ffcb77] text-center">Benefícios e Impacto do projeto</h2>
 
       <br />
 
@@ -26,8 +39,8 @@ export default function Home() {
         <li>
           <span className="font-bold">Redução da Produção de Plástico Global</span>
           <p>
-            De acordo com os dados de produção anual de plástico, a quantidade de plástico produzido tem aumentado significativamente desde 1950. 
-            A iniciativa CleanSea visa combater essa tendência crescente, recolhendo plástico diretamente dos oceanos e promovendo a 
+            De acordo com os dados de produção anual de plástico, a quantidade de plástico produzido tem aumentado significativamente desde 1950.
+            A iniciativa CleanSea visa combater essa tendência crescente, recolhendo plástico diretamente dos oceanos e promovendo a
             reciclagem e a redução do desperdício de plástico.
           </p>
         </li>
@@ -35,8 +48,8 @@ export default function Home() {
         <li>
           <span className="font-bold">Participação de Países no Despejo de Resíduos Plásticos</span>
           <p>
-            Diversos países contribuem para a emissão de plásticos nos oceanos. 
-            Com a implementação dos coletores CleanSea, esperamos reduzir significativamente a quantidade de plástico mal gerido e descartado, 
+            Diversos países contribuem para a emissão de plásticos nos oceanos.
+            Com a implementação dos coletores CleanSea, esperamos reduzir significativamente a quantidade de plástico mal gerido e descartado,
             especialmente nas regiões com maior contribuição.
           </p>
         </li>
@@ -44,7 +57,7 @@ export default function Home() {
         <li>
           <span className="font-bold">Destino do Plástico</span>
           <p>
-            Os dados mostram que uma grande proporção do lixo plástico é descartado ou mal gerido. 
+            Os dados mostram que uma grande proporção do lixo plástico é descartado ou mal gerido.
             O projeto CleanSea promove a reciclagem e a correta gestão dos resíduos plásticos, ajudando a diminuir a poluição marinha e terrestre.
           </p>
         </li>
@@ -52,7 +65,7 @@ export default function Home() {
         <li>
           <span className="font-bold">Poluição da Água nas Cidades</span>
           <p>
-            A poluição da água é um problema global que afeta tanto ambientes urbanos quanto naturais. 
+            A poluição da água é um problema global que afeta tanto ambientes urbanos quanto naturais.
             O projeto CleanSea, ao remover resíduos plásticos dos oceanos, contribui para a melhoria da qualidade da água e da saúde dos ecossistemas marinhos e costeiros.
           </p>
         </li>
